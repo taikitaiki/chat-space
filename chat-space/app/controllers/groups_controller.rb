486fef702c068141a8b4a167cd868a702338b1ac
Taiki <t.chinju0909@gmail.com>
2018-08-23 17:07:05 +0900

    add strong parameters method

--- chat-space/app/controllers/groups_controller.rb
+++ chat-space/app/controllers/groups_controller.rb
@@ -1,7 +1,18 @@
 class GroupsController < ApplicationController
   def new
+    @group = Group.new
+  end
+
+  def create
   end
 
   def edit
+  end
+
+  def update
+  end
+
+  def group_params
+    params.require(:post).permit(:name, { :user_ids =>[] })
   end
 end
