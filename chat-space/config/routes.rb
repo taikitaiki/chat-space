0c76a0133db4a40c7425384d5bb41a0cee85d10b
Taiki <t.chinju0909@gmail.com>
2018-08-23 21:51:31 +0900

    set nest between groups and message

--- chat-space/config/routes.rb
+++ chat-space/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index'
-  resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :udate]
+  root 'groups#index'
+
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :udate] do
+    resources :messages, only: [:index]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
