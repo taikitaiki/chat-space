fe003cbf6e664c21d0a1ee909b24b11eb5b7a7ab
Taiki <t.chinju0909@gmail.com>
2018-08-24 12:09:03 +0900

    definite 'index' and 'create' (message)

--- chat-space/app/controllers/messages_controller.rb
+++ chat-space/app/controllers/messages_controller.rb
@@ -1,4 +1,14 @@
 class MessagesController < ApplicationController
   def index
+    @messages = Message.all(message_params)
   end
+
+  def create
+  end
+
+  private
+  def message_params
+    params.permit(:content, :image, :group_id, :user_id)
+  end
+
 end
