51a838675bcc30a034a3ffaa4640bce5bb93afec
Taiki <t.chinju0909@gmail.com>
2018-09-05 17:35:29 +0900

    revise

--- chat-space/config/deploy.rb
+++ chat-space/config/deploy.rb
@@ -13,7 +13,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/taiki0909.pem']
+                  keys: ['/Users/taiki/.ssh/taiki0909.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
